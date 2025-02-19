264b7d10563b424cfc0b7d779f1f7d72886c9260
anders <anders@thermiq.net>
2025-02-19 22:52:19 +0100

    haos

--- thermiq_mqtt_listener/run.sh
+++ thermiq_mqtt_listener/run.sh
@@ -44,4 +44,4 @@ fi
 mkdir -p /usr/lib/php84/modules/opcache
 
 echo "Starting ThermIQ_MQTT_listener..."
-#exec php84 /share/thermiq/ThermIQ_MQTT_listener 
+exec php84 /share/thermiq/ThermIQ_MQTT_listener 
